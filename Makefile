cce4f8daf8cd0976bf4b1262364813586e4999fd
Connor Glosser <glosser1@gmail.com>
2013-05-31 15:45:49 -0400

    Added plot module and makefile to build

---
+++ Makefile
@@ -0,0 +1,18 @@
+FC      = gfortran
+FFLAGS  = -Wall -Wextra -march=native -O3 -fimplicit-none -fdefault-real-8
+#FFLAGS += -pedantic -fbounds-check -fmax-errors=1 -g
+FFLAGS += $(shell pkg-config --cflags plplotd-f95)
+LDFLAGS = $(shell pkg-config --libs plplotd-f95)
+LIBS    =
+
+COMPILE = $(FC) $(FFLAGS)
+LINK = $(FC) $(LDFLAGS)
+
+TARGET = ising.exe			# Name of final executable to produce
+OBJS = plot.o ising.o	# List of object dependencies
+
+$(TARGET): $(OBJS)
+	$(LINK) -o $@ $^ $(LIBS)
+
+%.o:%.f95
+	$(COMPILE) -c $<
---
+++ plot.f95
@@ -0,0 +1,64 @@
+module plot
+  use plplot
+  implicit none
+  private
+  
+  public plot_init, plot_close, plot_lattice
+
+contains
+
+  subroutine plot_init(latticeSize)
+    integer, intent(in) :: latticeSize 
+    call plsdev("xcairo")
+    call plinit()
+
+    !You can find default colors at
+    !http://plplot.sourceforge.net/docbook-manual/plplot-html-5.9.9/plcol0.html
+
+    !call plscol0(0, 255, 255, 255)  ! white
+    !call plscol0(1, 255, 0, 0)      ! red
+    !call plscol0(2, 255, 77, 0)     ! orange
+    !call plscol0(3, 255, 255, 0)    ! yellow
+    !call plscol0(4, 0, 255, 0)      ! green
+    !call plscol0(5, 0, 0, 255)      ! blue
+    !call plscol0(6, 0, 255, 255)    ! cyan
+    !call plscol0(7, 255, 0, 255)    ! magenta
+    !call plscol0(8, 128, 128, 128)  ! gray
+    !call plscol0(9, 0, 0, 0)        ! black
+
+    call plenv(0.0, latticeSize + 1.0, 0.0, latticeSize + 1.0, 0, 0)
+  end subroutine plot_init
+
+  subroutine plot_close()
+    call plspause(.false.)
+    call plend()
+  end subroutine plot_close
+
+  subroutine plot_lattice(lattice)
+    ! Draw a rectangular grid of up- and down-arrows corresponding to Ising
+    ! states. Because this redraws the entire screen, it is *very* slow for
+    ! Metropolis models - instead, consider a routine that only redraws the
+    ! sites that change.
+    integer, intent(in) :: lattice(:,:)
+    integer :: i, j
+    real :: x, y
+    !Assumes 1 corresponds to up-spin, -1 to down-spin.
+
+    call plclear()
+    do i = 1, size(lattice, 1)
+      do j = 1, size(lattice, 2)
+        x = i; y = j
+        if (lattice(i, j) .eq. 1) then
+          call plcol0(1)            !default red
+          call plpoin([x], [y], 30) !30 denotes up-arrow glyph
+        !else
+          !call plcol0(11)           !default cyan
+          !call plpoin([x], [y], 31) !31 denotes down-arrow glyph
+        end if
+      end do
+    end do
+
+    call plflush()
+  end subroutine plot_lattice
+
+end module plot
